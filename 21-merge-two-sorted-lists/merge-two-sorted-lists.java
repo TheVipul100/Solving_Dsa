01a0d6e4c0d8c7f2e51dc63b45513d72a2602011
TheVipul100 <141997597+TheVipul100@users.noreply.github.com>
2026-08-06 17:14:23 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (20.97%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -10,23 +10,39 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-      List<Integer> st = new ArrayList<>();
-     ListNode t = list1;
-     while(t != null){
-        st.add(t.val);
-        t = t.next;
-        }
 
-    for (ListNode x = list2; x != null; x = x.next) st.add(x.val);
-  
-    Collections.sort(st);
-
-    ListNode dummy = new ListNode(-1);
+            ListNode dummy = new ListNode(-1);
     ListNode curr = dummy;
-    for (int val : st) {
-        curr.next = new ListNode(val);
+    while (list1 != null && list2 != null) {
+        if (list1.val <= list2.val) {
+            curr.next = list1;
+            list1 = list1.next;
+        } else {
+            curr.next = list2;         // soln in O(N)
+            list2 = list2.next;
+        }
         curr = curr.next;
     }
+    curr.next = (list1 != null) ? list1 : list2;
     return dummy.next;
+
+    //   List<Integer> st = new ArrayList<>();
+    //  ListNode t = list1;
+    //  while(t != null){
+    //     st.add(t.val);
+    //     t = t.next;
+    //     }
+
+    // for (ListNode x = list2; x != null; x = x.next) st.add(x.val);
+  
+    // Collections.sort(st);   dominating so o(nlogn)
+
+    // ListNode dummy = new ListNode(-1);
+    // ListNode curr = dummy;
+    // for (int val : st) {
+    //     curr.next = new ListNode(val);  
+    //     curr = curr.next;
+    // }
+    // return dummy.next;
     }
 }
