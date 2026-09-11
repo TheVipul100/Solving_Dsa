0835f26e01f01de2f8b9da5c3486883862c487ce
TheVipul100 <141997597+TheVipul100@users.noreply.github.com>
2026-09-11 14:12:56 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (37.34%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -6,6 +6,15 @@ class Solution {
         while(low<=high)
         { 
             int mid=(low+high)/2;
+                //when the search space is sorted in case[4,5,6,0,1,2] so 0 to 2 the array is sorted directly then take low as your minimum in that case 
+
+                if(nums[low] <= nums[high])
+                {
+                    min=Math.min(min,nums[low]);
+                    break; // no need to perform further binary search on [0,1,2] directly 0 is the ans;
+                }
+
+
             // left sprted
             if(nums[low]<=nums[mid])
             {
